0f6b81cdb83d0037329511bc1c75996a4986bbc2
juanmarr <juanmarrugo90@gmail.com>
2023-08-07 17:38:05 -0400

    commiting my work and sync with kerbs

--- src/page/Editvolunteer.jsx
+++ src/page/Editvolunteer.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router"
 import {Link} from 'react-router-dom'
 
-//when user clicks edit volunterr next to name
+//when user clicks edit volunterr next to name //break into components
 export default function Editvolunteer() {
     const { id } = useParams
     //edit students same page design as add/ include autofill data... tbd
