c1db857f817a25a91249e00fda1a9a4b34bd2e18
juanmarr <juanmarrugo90@gmail.com>
2023-08-14 19:07:39 -0400

    .

--- src/components/AdminComp.jsx
+++ src/components/AdminComp.jsx
@@ -9,7 +9,7 @@ export default function AdminComp() {
         <div className='App'>
         <div className="options">
                 <button><Link to='/admin/managevolunteers' className='link'><b>Manage volunteers</b></Link></button>
-                <button><Link to='/' className='link'><b>Manage oppprtunities</b></Link></button>
+                <button><Link to='/admin/manageopportunities' className='link'><b>Manage oppprtunities</b></Link></button>
             </div>
         </div>
         </>
